1528a178ae5d975dac80c15d4a9db0f769ef8e21
estudiante <estudiante@oretania-PC>
2025-10-17 08:58:43 +0200

    version 1.0

---
+++ Bienvenida/src/es/iesoretania/saludo/Hola.java
@@ -0,0 +1,15 @@
+package es.iesoretania.saludo;
+
+public class Hola {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		System.out.println(" Hola Mundo !! ");
+
+		
+		
+	}
+
+}
